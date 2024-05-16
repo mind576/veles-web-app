dd0c63df3cd0321245f28a32af96e597502ffb36
ewan <ivangoncharov@Mac-mini-Ivan.local>
2024-05-16 12:41:18 +0300

    phone validators added

--- src/schemas.py
+++ src/schemas.py
@@ -1,15 +1,41 @@
 import uuid
 
 from fastapi_users import schemas
+from pydantic import BaseModel,field_validator
+from pydantic.types import constr
+
+
 
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    pass
+    position: str 
+    company: str
+    phone_number: str
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    position: str 
+    company: str
+    phone_number: str 
+    @field_validator('phone_number')
+    @classmethod
+    def phone_number_must_contain_plus(cls, v: str) -> str:
+        prefix = '+7'
+        if not cls.phone_number.starts_with(prefix):
+            raise ValueError('Must contain a +7')
+        return v.title()
 
+
+#str.startswith(prefix[, start[, end]])
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    position: str 
+    company: str
+    phone_number: str
+    @field_validator('phone_number')
+    @classmethod
+    def phone_number_must_contain_plus(cls, v: str) -> str:
+        prefix = '+7'
+        if not cls.phone_number.starts_with(prefix):
+            raise ValueError('Must contain a +7')
+        return v.title()
