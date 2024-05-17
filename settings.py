54c879fd1d529ee14c9290b28b464551807e0eb9
ewan <ivangoncharov@Mac-mini-Ivan.local>
2024-05-17 10:32:28 +0300

    test1

--- settings.py
+++ settings.py
@@ -9,14 +9,13 @@ root_dir = pathlib.Path(__file__).parent.resolve()
 # DOTENV FILE
 config = dotenv_values(f"{root_dir}/.env")
 
-# When it's building it feeds from .env  docker url
+# Dev usage
 if config['POSTGRES_DOCKER_BUILD'] == 'True':
     PG_HOST = config["POSTGRES_HOST_DOCKER"]
 elif config['POSTGRES_DOCKER_BUILD'] == 'False':
     PG_DOCKER_RUN_DEV_MODE = config['DEV_DOKER_POSTGRES_CMD']
     PG_HOST = config["POSTGRES_HOST_LOCAL"]
-    
-    # If you prefer on the start this cmd will execute - run docker container with postgres instance
+
     # os.system(PG_DOCKER_RUN_DEV_MODE)             # DEV_DOKER_POSTGRES_CMD='docker run --name db_postgres  -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -d postgres:latest'
 
     # time.sleep(10)
